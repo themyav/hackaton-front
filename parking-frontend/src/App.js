f9e2a107de7a5322f16f599ca178018f6c23e080
viktoriabatomunkueva <torichan373737@gmail.com>
2025-03-30 00:02:37 +0300

    app fixed

--- parking-frontend/src/App.js
+++ parking-frontend/src/App.js
@@ -8,24 +8,38 @@ import MyParkingsPage from "./pages/MyParkingsPage.tsx";
 import ProfilePage from "./pages/ProfilePage.tsx";
 import ReportPage from "./pages/ReportPage.tsx";
 import MapPage from "./pages/MapPage.tsx";
+import {Toaster} from 'react-hot-toast';
+
+import BookingModal from "./components/BookingModal.tsx";
+import ParkingSpot from "./components/ParkingSpot.tsx";
 
 
 function App() {
     return (
-        <Router>
-            <div className="App">
-                <Routes>
-                    <Route path="/" element={<HomePage/>}/>
-                    <Route path="/login" element={<StartPage/>}/>
-                    <Route path="/home" element={<HomePage/>}/>
-                    <Route path="/profile" element={<ProfilePage/>}/>
-                    <Route path="/my-parkings" element={<MyParkingsPage/>}/>
-                    <Route path="/reports" element={<ReportPage/>}/>
-                    <Route path="/map" element={<MapPage/>}/>
-
-                </Routes>
-            </div>
-        </Router>
+        <>
+            <Toaster
+                position="top-center"
+                toastOptions={{
+                    duration: 1500,
+                    style: {
+                        fontSize: '20px',
+                    },
+                }}
+            />
+            <Router>
+                <div className="App">
+                    <Routes>
+                        <Route path="/" element={<MapPage/>}/>
+                        <Route path="/login" element={<StartPage/>}/>
+                        <Route path="/home" element={<HomePage/>}/>
+                        <Route path="/profile" element={<ProfilePage/>}/>
+                        <Route path="/my-parkings" element={<MyParkingsPage/>}/>
+                        <Route path="/reports" element={<ReportPage/>}/>
+                        <Route path="/map" element={<MapPage/>}/>
+                    </Routes>
+                    </div>
+            </Router>
+        </>
     );
 }
 
