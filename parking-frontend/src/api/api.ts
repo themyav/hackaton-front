8977ec3f3aa4ec327256992d266d7325acad1399
Kristina Kravtsova <335051@edu.itmo.ru>
2025-03-29 14:25:42 +0300

    Login and register pages

---
+++ parking-frontend/src/api/api.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const loginUser = async (userData) => {
+    console.log("Login operation")
+}
+
+export const registerUser = async (userData) => {
+    console.log("Register operation")
+}
